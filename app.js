0f698bb98bdff7e28a8dddfbf488e95a871490b3
vietdung97 <ngovietdung0112@gmail.com>
2023-11-16 16:15:36 +0700

    Update

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function handleSubmit(e) {
       receipt_email: "abc@gmail.com",
     },
   });
-
+  alert(JSON.stringify(error))
   // This point will only be reached if there is an immediate error when
   // confirming the payment. Otherwise, your customer will be redirected to
   // your `return_url`. For some payment methods like iDEAL, your customer will
